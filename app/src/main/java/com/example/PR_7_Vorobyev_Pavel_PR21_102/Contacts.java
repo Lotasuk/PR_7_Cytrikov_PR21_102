c1711a08a83ca3803aaef81798fc321a8f0711df
Your Name <cytrikov.m@Mail.ru>
2023-06-23 16:53:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/PR_7_Vorobyev_Pavel_PR21_102/Contacts.java
@@ -0,0 +1,14 @@
+package com.example.PR_7_Vorobyev_Pavel_PR21_102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Contacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
